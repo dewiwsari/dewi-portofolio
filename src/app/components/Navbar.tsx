e9d0774278579993c0b7fded13720266669767f2
dewiwulansarii <dewiwulansari627@gmail.com>
2026-06-19 23:58:49 +0700

    change the name

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -43,7 +43,7 @@ export function Navbar() {
               className="hidden sm:block text-[#0F172A] font-semibold text-sm tracking-tight"
               style={{ fontFamily: "'Plus Jakarta Sans',sans-serif" }}
             >
-              Dewi's Portfolio
+              Dewi Wulansari
             </span>
           </a>
 
